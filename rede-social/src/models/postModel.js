b6dc3b4c1ce2cc33a9c991a8dfc1ac8c384e3ae6
Erickify <erick.silva@sptech.school>
2025-05-08 01:48:37 -0300

    Adicionando Posts

--- rede-social/src/models/postModel.js
+++ rede-social/src/models/postModel.js
@@ -16,7 +16,7 @@ function postar(descPost, idUsuario) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO tbPost (descPost, fkUsuario, imagensPost) VALUES ('${descPost}', ${idUsuario});    `;
+        INSERT INTO tbPost (descPost, fkUsuario) VALUES ('${descPost}', ${idUsuario});    `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
